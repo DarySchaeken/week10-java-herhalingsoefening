9b6cf5263bbd62ec449882beea7e41e69c2164a6
DarySchaeken <dary.schaeken@student.pxl.be>
2017-12-06 21:23:01 +0100

    Mappers implemented

--- src/ticketSystem/EventMapper.java
+++ src/ticketSystem/EventMapper.java
@@ -4,8 +4,15 @@ public class EventMapper implements Mapper<Event> {
 
 	@Override
 	public Event map(String[] dataInput) {
-		// TODO implementation
-		return null;
+		String dateAndTime = dataInput[1];
+		StringBuilder date = new StringBuilder(dateAndTime.substring(0, 8));
+		StringBuilder time = new StringBuilder(dateAndTime.substring(8));
+		date.insert(4, '/');
+		date.insert(2, '/');
+		time.insert(2, ':');
+
+		return new Event(dataInput[0], dataInput[2], date.toString(), time.toString(), dataInput[3],
+				Double.parseDouble(dataInput[4]), dataInput[5]);
 	}
 
 }
--- src/ticketSystem/UserMapper.java
+++ src/ticketSystem/UserMapper.java
@@ -4,14 +4,11 @@ public class UserMapper implements Mapper<User> {
 
 	@Override
 	public User map(String[] dataInput) {
-		String id = dataInput[0];
-		String name = dataInput[1];
-		String firstName = dataInput[2];
 		StringBuilder birthDate = new StringBuilder(dataInput[3]);
 		birthDate.insert(4, '/');
 		birthDate.insert(2, '/');
 		
-		return new User(id, firstName, name, birthDate.toString());
+		return new User(dataInput[0], dataInput[1], dataInput[2], birthDate.toString());
 	}
 	
 }
